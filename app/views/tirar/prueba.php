8d44045dbd311c94035ca789b7f3c07e2a743f53
rtorresmtz <rtorresmtz@hotmail.com>
2014-12-09 22:50:26 -0600

    Bootstrap modal test

--- app/views/tirar/prueba.php
+++ app/views/tirar/prueba.php
@@ -1,58 +1,54 @@
 <!DOCTYPE html>
-  
-<html lang="en">
-
+<html>
 <head>
-
-<style type="text/css">
-
-div#msgBox{
-  border-style: solid;
-  border-width: 1px;
-  border-color: lightgray;
-  height:200px;
-  width:300px;
-  line-height: 1px;
-
-}
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
   
-div#msgTitle{
-   color:gray;
-    margin-left: auto;
-    margin-right: auto;
-
-}
-
- 
- 
-</style>
- 
-<head>
-
+</head>
 <body>
 
-<div id="msgBox">
+ <br> 
+<a href="#" class="btn btn-lg btn-success"
+   data-toggle="modal"
+   data-target="#basicModal">Click to open Modal</a>
 
-    <div id="msgTitle" >
-      <h4>
-           INFORMATION
-      </h4>
-<hr>
+
+
+<div class="modal fade" id="basicModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&amp;times;</button>
+                <h4 class="modal-title" id="myModalLabel">
+                    Modal title
+                </h4>
+            </div>
+            <div class="modal-body">
+                <h3>Modal Body</h3>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
     </div>
-
-    <div id="msgContent" >
-      
-      <h4>
-          Are you sure to delete the item?
-      </h4>
-
-
-      <div id="msgButtons" style="align:left">
-        <a href="">Accept</a>
-        <a href="">Cancel</a>
-      </div>
-    </div>
-
+  </div>
 </div>
 
+
+</div> 
+
+<script>
+
+$(#basicModal).on('click',funcition(){
+  
+ alert('hola');
+
+});
+
+</script>
+
+<script  src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script  src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 </body>
+</html>
